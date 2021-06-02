c22c04aa9f3b2c8516986f7e88217fbb80351731
Arun Endapally <arun.e.kumar@gmail.com>
2021-06-02 17:56:11 +0530

    abstract class example

---
+++ InterviewQuestions/AbstractClassExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InterviewQuestions
+{
+    public abstract class MyAbstractClass
+    {
+        public abstract void Print();
+
+        public void SayHello()
+        {
+            Console.WriteLine("This is Abstract class");
+        }
+    }
+
+    public class DerivedClass : MyAbstractClass
+    {
+        public static void Main()
+        {
+            MyAbstractClass c = new DerivedClass();
+            c.Print();
+            c.SayHello();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("This is derived class");
+        }
+    }
+}
