5d6a16be7636dfc74b32b541ead3c885cf004922
Arun Endapally <arun.e.kumar@gmail.com>
2021-06-02 18:55:10 +0530

    extension method examples

---
+++ InterviewQuestions/ExtensionMethodExample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace InterviewQuestions
+{
+    public class ExtensionMethodExample
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            Console.WriteLine(i.IsGreaterThan5());
+
+            i = 8;
+            Console.WriteLine(i.IsGreaterThan5());
+
+            i = 15;
+            Console.WriteLine(i.IsDivisibleByGivenNumber(5));
+
+            i = 11;
+            Console.WriteLine(i.IsDivisibleByGivenNumber(5));
+        }
+    }
+
+    public static class MyExtensionMethod
+    {
+        public static bool IsGreaterThan5(this int i)
+        {
+            if (i > 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool IsDivisibleByGivenNumber(this int i, int j)
+        {
+            if (i % j == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
